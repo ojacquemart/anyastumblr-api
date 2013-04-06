e43e48d13bb58306d537d6885bc9f81021ff97e8
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-04-06 21:59:14 +0200

    Add navigation with keys z,q,s,d. FPS style. Issue #5.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,8 +3,27 @@
  */
 function siteGifsController($scope, $http) {
 
+    $scope.currentSiteIndex = 0;
     $scope.page = null;
     $scope.lastPageInfos = null;
+
+    $scope.updateCurrentSiteIndex = function() {
+
+        function searchSiteIndex() {
+            var sitesSize = $scope.sites.length;
+
+            for (var i = 0; i < sitesSize; i++) {
+                var site = $scope.sites[i];
+                if (site.id == $scope.siteId) {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        $scope.currentSiteIndex = searchSiteIndex();
+    }
 
     $scope.storeImages = function (data) {
         $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -22,9 +41,13 @@ function siteGifsController($scope, $http) {
         $http.get("sites/" + $scope.siteId + "/gifs")
             .success(function (data) {
                 $("#topics-select").blur();
-                $scope.lastPageInfos = null;
                 $scope.storeImages(data);
+
+                // Reset last page infos to reinit binding.
+                $scope.lastPageInfos = null;
                 $scope.loadLastPageInfos();
+
+                $scope.updateCurrentSiteIndex();
             });
 
     };
@@ -55,14 +78,37 @@ function siteGifsController($scope, $http) {
         }
     };
 
+    $scope.loadSiteImages = function(siteIndex) {
+        var nextIndex = $scope.currentSiteIndex + siteIndex;
+        if (nextIndex >= 0 && nextIndex < $scope.sites.length) {
+            $scope.currentSiteIndex = nextIndex;
+            // to bind topic in select.
+            $scope.siteId = $scope.sites[nextIndex].id;
+            $scope.loadImages();
+        }
+    }
+
     $scope.handleKeypress = function(key) {
-        // left = 37, right = 39
+        // left = 37, right = 39, q = 81, d = 68, z = 90, s = 83
         switch (key) {
-            case 37: // Previous page = left
+            // Previous page = left | q
+            case 37:
+            case 81:
                 $scope.loadPreviousPage();
                 break;
-            case 39: // Next page = right
+            // Next page = right | d
+            case 39:
+            case 68:
                 $scope.loadNextPage();
+                break;
+                $scope.siteId = $scope.sites[$scope.currentSiteIndex].id;
+            // Previous site = up
+            case 90:
+                $scope.loadSiteImages(-1);
+                break;
+            // Next site = down
+            case 83:
+                $scope.loadSiteImages(1);
                 break;
         }
     };
@@ -72,9 +118,7 @@ function siteGifsController($scope, $http) {
     //-------------------
     $http.get('sites').success(function (data) {
         $scope.sites = data;
-        $scope.siteId = data[0].id; // to initialize default topic in select.
-
-        $scope.loadImages();
+        $scope.loadSiteImages(0);
     });
 
 }

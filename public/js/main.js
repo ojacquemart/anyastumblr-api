02e487777a7df7e99ad815bec0a825db13c7bd20
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-04-16 00:02:19 +0200

    Add String.format function to main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,25 @@
-$(function() {
+/**
+ * Format method, java like.
+ * @return {String}
+ */
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        if (typeof args[number] != 'undefined') {
+            return args[number];
+        }
 
-    // Search
-    var search = function() {
+        return "";
+    });
+};
+
+$(function () {
+    // FIXME: to change... for tweets streaming.
+    var search = function () {
         console.log(Router.controllers.Tweets.stream().url)
         var stream = new EventSource(Router.controllers.Tweets.stream().url)
 
-        $(stream).on('message', function(e) {
+        $(stream).on('message', function (e) {
             var tweet = JSON.parse(e.originalEvent.data)
             console.log(e.originalEvent)
             console.log(tweet)

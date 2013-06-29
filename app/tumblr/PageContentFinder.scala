bbf7a13618c2ec694dc5e20300ec4d66f4fca6c6
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-06-29 09:39:39 +0200

    Remove dead code.

--- app/tumblr/PageContentFinder.scala
+++ app/tumblr/PageContentFinder.scala
@@ -41,33 +41,6 @@ case class PageContentFinder(site: Site, pageNumber: Option[Int])  {
     page
   }
 
-  def getContentFromMongo(): Future[Option[Page]] = {
-    Logger.info("getContentFromMongo")
-    val futureOptionPage = PageDao.findHeadByTopicIdAndPageOffset(site.id, pageNumber.get)
-    // TODO: see to avoid the flatMap and use a map instead.
-    futureOptionPage.flatMap(checkMongoContent(_))
-  }
-
-  def checkMongoContent(optionPage: Option[Page]): Future[Option[Page]] = {
-    Logger.debug("checkMongoContent object... retrieve it from site or update existing one.")
-    // Page doesn't exist in mongo, retrieve it from hfr else return the future optionPage.
-    optionPage match {
-      case None => getContentFromSite()
-      case Some(page: Page) => {
-        val site = SiteDao.findSite(page.siteId)
-        val currentUrl = new PageNumberResolver(site, Some(page.pageNumber)).resolve()._1
-        page.link = Some(Link.get(currentUrl, site.name, page.pageNumber))
-
-        future {
-          future {
-            val existingPage = getPageFromSite()
-            PageDao.update(existingPage)
-          }
-          optionPage
-        }
-      }
-    }
-  }
 }
 
 object PageContentFinder {

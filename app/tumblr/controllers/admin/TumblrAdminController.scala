d6f4ad907e78ce9b4a3a802f00a77ac9c352ea29
unknown <s815484@UC424247.siege.axa-fr.intraxa>
2013-07-24 11:30:22 +0200

    Missing admin controller.

---
+++ app/tumblr/controllers/admin/TumblrAdminController.scala
@@ -0,0 +1,12 @@
+package tumblr.controllers.admin
+
+import play.api.mvc._
+
+object TumblrAdminController extends Controller with securesocial.core.SecureSocial {
+
+  def index = SecuredAction { request =>
+      Ok(views.html.admin())
+  }
+
+
+}
